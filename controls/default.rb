6685843cfdbbe4d1002e74363c4b88e444340d8d
akshayparvatikar174 <apravati@progress.com>
2025-03-27 20:47:56 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -4,7 +4,6 @@ control 'check-test-file-present' do
 
   # Path to the file you want to check
   file_path = '/home/polyfil/sandbox.html'
-  file_path1 = '/home/polyfil/sandbox.html'
 
   describe file(file_path) do
     it { should exist }  # This checks if the file exists at the specified path
