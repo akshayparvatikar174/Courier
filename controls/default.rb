fdd4133e87d3710c654b0950469a289999c4bdf2
akshayparvatikar174 <apravati@progress.com>
2025-03-27 20:49:55 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -8,16 +8,9 @@ control 'check-test-file-present' do
   describe file(file_path) do
     it { should exist }  # This checks if the file exists at the specified path
   end
-end
-
-control 'check-test-file-present' do
-  title 'Ensure the akshay.html file exists'
-  desc 'Check if the /home/polyfil/akshay.html file exists on the system'
-
-  # Path to the file you want to check
-  file_path1 = '/home/polyfil/akshay.html'
+file_path1 = '/home/polyfil/akshay.html'
 
   describe file(file_path1) do
     it { should exist }  # This checks if the file exists at the specified path
   end
-end
+end 
